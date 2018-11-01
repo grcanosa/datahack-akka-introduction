fd8e78bbe98e25a2285d83e28b74317dc67da4ab
Rafael Garrote Hernández <rafaelgarrote@MacBook-Pro-de-Rafael.local>
2018-11-01 13:33:10 +0100

    Clase 2018/10/31 Ejercicio Introduction

--- src/main/scala/com/datahack/akka/introduction/Boot.scala
+++ src/main/scala/com/datahack/akka/introduction/Boot.scala
@@ -1,5 +1,29 @@
 package com.datahack.akka.introduction
 
+import akka.actor.{ActorRef, ActorSystem, Props}
+import com.datahack.akka.introduction.acotors.Student.PerformAnAdviceRequest
+import com.datahack.akka.introduction.acotors.{Student, Teacher}
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
+
 object Boot extends App {
 
+  val actorSystem = ActorSystem("UniversityMessageSystem")
+
+  implicit val executionContext: ExecutionContextExecutor = actorSystem.dispatcher
+
+  val teacherActorRef: ActorRef =
+    actorSystem.actorOf(Props[Teacher], "teacher")
+  val studentActorRef: ActorRef =
+    actorSystem.actorOf(Props(classOf[Student], teacherActorRef), "student")
+
+  actorSystem.scheduler
+    .schedule(
+      5 seconds,
+      15 seconds,
+      studentActorRef,
+      PerformAnAdviceRequest)
+
+  sys.addShutdownHook(actorSystem.terminate())
 }
